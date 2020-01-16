1a61453d684527179eec7cfbe610203d796db7c9
Syed Abbas <s01061178@acad.tri-c.edu>
2020-01-16 10:56:06 -0500

    Created Lab1

---
+++ Lab1/Lab1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab1
+//
+//  Created by Syed ali Abbas on 1/15/20.
+//  Copyright © 2020 Syed ali Abbas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World From Syed!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
