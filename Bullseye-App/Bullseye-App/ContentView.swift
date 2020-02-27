4ee8cff06f39f4ed81a60b38b6092ce04579a25b
Syed Abbas <s01061178@acad.tri-c.edu>
2020-02-27 13:10:19 -0500

    Lab 3 incomplete HighScore method not working!

--- Bullseye-App/Bullseye-App/ContentView.swift
+++ Bullseye-App/Bullseye-App/ContentView.swift
@@ -11,6 +11,10 @@ import SwiftUI
 struct ContentView: View {
     @State var alertIsVisible: Bool = false
     @State var sliderValue: Double = 50.0
+    @State var target: Int = Int.random(in: 1...100)
+    @State var score = 0
+    @State var round = 1
+    @State var highScore = 0
     
     var body: some View {
         VStack {
@@ -18,8 +22,8 @@ struct ContentView: View {
             
             // Target Row
             HStack {
-                Text("Put the bullseye as close as you can to:")
-                Text(/*@START_MENU_TOKEN@*/"100"/*@END_MENU_TOKEN@*/)
+                Text("Put the bullseye as close as you can to: ")
+                Text("\(target)")
                     
             }
             Spacer()
@@ -27,7 +31,7 @@ struct ContentView: View {
             // Slider row
             HStack{
                 Text("1")
-                Slider(value: self.$sliderValue, in: 1...100)
+                Slider(value: $sliderValue, in: 1...100)
                 Text("100")
             }
             Spacer()
@@ -37,37 +41,96 @@ struct ContentView: View {
             Button(action: {
                 print("Button Pressed!")
                 self.alertIsVisible = true
+                
             }) {
                 Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
             }
             .alert(isPresented: $alertIsVisible) { () -> Alert in
-                var roundedValue: Int = Int(self.sliderValue.rounded())
-                return Alert(title: Text("Hello There"),
-                             message: Text("The Slider value is \(roundedValue)."),
-                             dismissButton: .default(Text("Awesome!")))
+                let roundedValue: Int = Int(sliderValue.rounded())
+                return Alert(title: Text(alertTitle()),
+                             message: Text("The Slider value is \(roundedValue). \n" +
+                                "You scored \(PointsForCurrentRound()) points this round."
+                ),         dismissButton: .default(Text("Awesome!")) {
+                    self.score = self.score + self.PointsForCurrentRound()
+                    self.target = Int.random(in: 1...100)
+                    self.round = self.round + 1
+                    })
             }
             Spacer()
             
             // Score row
             HStack {
-                Button(action: {}) {
+                Button(action: {
+                    self.startNewGame()
+                }) {
                 Text("Start Over")
             }
                 Spacer()
+                Text("High Score:")
+                Text("\(highScore)")
+                Spacer()
                 Text("Score:")
-                Text("999999")
+                Text("\(score)")
                 Spacer()
                 Text("Round:")
-                Text("999")
+                Text("\(round)")
                 Spacer()
                 Button(action: {}) {
-                    Text("Info")
+                Text("Info")
                           
                 }
         }
             .padding(.bottom, 20)
     }
+        
 }
+    func sliderValueRounded() -> Int {
+        return Int(sliderValue.rounded())
+    }
+    
+    
+    func amountOff() -> Int {
+        abs(target - sliderValueRounded())
+    }
+    
+    
+    func PointsForCurrentRound() -> Int {
+        
+        
+        let maximumScore = 100
+        let difference = amountOff()
+        let bonus: Int
+        if difference == 0 {
+            bonus = 100
+        } else if difference == 1 {
+            bonus = 50
+        } else {
+            bonus = 0
+        }
+        return maximumScore - difference + bonus
+        
+    }
+    func alertTitle() -> String {
+        let difference = amountOff()
+        let title: String
+        if difference == 0 {
+            title = "Great Job"
+        } else if difference < 5{
+            title = "Close"
+        }else if difference <= 10{
+            title = "Little more Effort Needed"
+        } else {
+            title = "You are Better than that"
+        }
+        return title
+    }
+    
+    func startNewGame () {
+        score = 0
+        round = 1
+        sliderValue = 50.0
+        target = Int.random(in: 1...100)
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
